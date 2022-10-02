4056add825262833f69d2f767964fc4671056ab3
Major2000 <majormadobe@gmail.com>
2022-10-02 12:43:33 +0300

    slice added in test units

--- csv2json_test.go
+++ csv2json_test.go
@@ -9,3 +9,30 @@ import (
 	"reflect"
 	"testing"
 )
+
+func Test_getFileData(t *testing.T) {
+
+	// defining test slice
+	tests := []struct {
+		name	string // nam of the test
+		want 	inputFile // the input instance to return
+		wantErr	bool // whether or not want an error
+		osArgs	[]string // command arguments for the test
+	}{
+
+		// declaring each unit test input and output data
+		{"Default parameters", inputFile{"test.csv", "comma", false}, false, []string{"cmd", "test.csv"}},
+		{"No parameters", inputFile{}, true, []string{"cmd"}},
+		{"semicolon enabled", inputFile{"test.csv", "semicolon", false}, false, []string{"cmd", "--separator=semicolon", "test.csv"}},
+		{"Pretty enabled", inputFile{"test.csv", "comma", true}, false, []string{"cmd", "--pretty", "test.csv"}},
+		{"Pretty and semicolon enabled", inputFile{"test.csv", "semicolon", true}, false, []string{"cmd", "--pretty", "--separator=semicolon", "test.csv"}},
+		{"Separator not identified", inputFile{}, true, []string{"cmd", "--separator=pipe", "test.csv"}},
+	}
+
+	// iterating over the slice
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T)) {
+			// saving original os.Args reference
+		}
+	}
+}
